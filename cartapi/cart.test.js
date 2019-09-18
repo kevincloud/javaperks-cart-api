a8e8a34f64dc7b194cf6000c8c8f9635e1bf0da7
Kevin Cochran <kcochran@hashicorp.com>
2019-09-18 09:06:28 -0400

    update

---
+++ cartapi/cart.test.js
@@ -0,0 +1,28 @@
+// const blobStore = require('./cart');
+// const express = require('express');
+// const moxios = require('moxios');
+// const request = require('supertest');
+
+// const initDynamoDB = (
+//     mockDocumentClient = {
+//         query: jest.fn(() => Promise.resolve()),
+//         put:adf,
+//         update:asdf,
+//         delete:asdf
+//     }
+// ) => {
+//     const app = express();
+//     app.use(mockDocumentClient);
+//     return app;
+// };
+
+// describe('POST /cart', () => {
+//     test('', async() => {
+//         const mockDocumentClient = {
+//             query: jest.fn(() => Promise.resolve())
+//         };
+//         const app = initDynamoDB(mockDocumentClient);
+//         await request(app).get('/cart?sessionId=s8d8gj3n2b8v8b45is');
+        
+//     });
+// });
